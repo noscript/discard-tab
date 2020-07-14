a11321610be4893f1d1a03fff340446c696cdb51
Sergey Vlasov <sergey@vlasov.me>
2020-07-14 21:45:12 +0300

    discard to nearest

--- background.js
+++ background.js
@@ -4,18 +4,27 @@ browser.contextMenus.create({
   contexts: ["tab"]
 });
 
-var upid = 0;
+function discard_to_nearest(tab_to_discard) {
+  browser.tabs.query({ active: false, currentWindow: true, discarded: false }).then((non_discarded_tabs) => {
+    if (non_discarded_tabs.length == 0) {
+      return;
+    }
+
+    var nearest_tab = non_discarded_tabs.reduce(function(prev, curr) {
+      return (Math.abs(curr.index - tab_to_discard.index) < Math.abs(prev.index - tab_to_discard.index) ? curr : prev);
+    });
+
+    browser.tabs.update(nearest_tab.id, { active: true }).then((done) => {
+      browser.tabs.discard(tab_to_discard.id);
+    })
+  });
+}
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "unload-tab") {
-    let active = browser.tabs.query({active: true, currentWindow: true}).then((tabs) => {
-      if (tabs[0].id == tab.id){
-      let next = browser.tabs.query({index:tabs[0].index + 1, currentWindow: true}).then((tab2) => {
-        var ind = tab2[0].id
-        var updating = browser.tabs.update(ind, { active: true }).then((done) => {
-          browser.tabs.discard(tab.id);
-        })
-      })
+    browser.tabs.query({ active: true, currentWindow: true }).then((current_tabs) => {
+      if (current_tabs[0].id == tab.id) {
+        discard_to_nearest(current_tabs[0]);
       } else {
         browser.tabs.discard(tab.id);
       }
@@ -25,13 +34,8 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
 
 browser.commands.onCommand.addListener(function(command) {
   if (command == "unload-tab-c") {
-    let active = browser.tabs.query({active: true, currentWindow: true}).then((tabs) => {
-      let next = browser.tabs.query({index:tabs[0].index + 1, currentWindow: true}).then((tab2) => {
-        var ind = tab2[0].id
-        var updating = browser.tabs.update(ind, { active: true }).then((done) => {
-          browser.tabs.discard(tabs[0].id);
-        })
-      })
+    browser.tabs.query({ active: true, currentWindow: true }).then((current_tabs) => {
+      discard_to_nearest(current_tabs[0]);
     })
   }
 });
